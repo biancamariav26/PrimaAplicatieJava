ed5452a12199f3cb6034d618f64618e3f51844fd
Bianca Maria <biancamariav26@gmail.com>
2020-12-29 11:05:08 +0200

    Modficare Main.java stergere comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,8 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
-    //JAVA_HOME
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
     }
 }
